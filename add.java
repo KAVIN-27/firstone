16d9fb6503def30df522dc20f6e830c20a708d48
KAVIN-27 <70503987+KAVIN-27@users.noreply.github.com>
2023-05-18 12:47:08 +0530

    changed variable b

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add {
     public static void main(String args[]) {
         int a = 5;
-        int b = 7;
+        int b = 5;
         int sum = a + b;
         System.out.println(sum);
 
