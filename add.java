9a010a038939ca6cb0de51c45b25799f10aa0536
unknown <2150717@cognizant.com>
2023-05-18 12:53:28 +0530

    changed a value

--- add.java
+++ add.java
@@ -1,6 +1,6 @@
 public class add {
     public static void main(String args[]) {
-        int a = 5;
+        int a = 9;
         int b = 5;
         int sum = a + b;
         System.out.println(sum);
