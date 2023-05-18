f2fbe2a2fbb6c4711f7dbea610748983e30978a6
unknown <2150717@cognizant.com>
2023-05-18 13:06:00 +0530

    changed the operation

--- add.java
+++ add.java
@@ -1,9 +1,9 @@
 public class add {
     public static void main(String args[]) {
 
-        int a = 22;
+        int a = 2;
         int b = 4;
-        int sum = a + b;
+        int sum = a *b;
         System.out.println(sum);
 
     }
