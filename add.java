62e879186188a65cec848917deafa5ff97eda323
KAVIN-27 <70503987+KAVIN-27@users.noreply.github.com>
2023-05-18 12:53:47 +0530

    changed a to 8

--- add.java
+++ add.java
@@ -1,6 +1,6 @@
 public class add {
     public static void main(String args[]) {
-        int a = 5;
+        int a = 8;
         int b = 5;
         int sum = a + b;
         System.out.println(sum);
